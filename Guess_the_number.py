ebc64fab7985c292f28d7ce95ebc9c5644f60129
Kowalski42 <sealenochka@gmail.com>
2022-07-26 01:36:09 +0300

    Some corrections

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,12 +1,28 @@
 from random import *
 from math import *
-upper_border = int(input('Введи любое число от 1 до 100, в рамках которого будет выбрано случайное: '))
-print('Угадай, какое число было загадано? ', end='')
-goal_number = randint(0, upper_border)
-attempt = ceil(log2(upper_border))
+
+
+def is_valid_digit(string, border):
+    return string.isdigit() and 1 <= int(string) <= border
+
+
+def get_number(text):
+    while True:
+        string = input(text)
+        if is_valid_digit(string, 100):
+            return int(string)
+        else:
+            print('Некорректный ввод!', end='')
+
+
+upper_border = get_number('Введи любое число от 1 до 100, в рамках которого будет выбрано случайное: ')
+goal_number, attempt = randint(0, upper_border), ceil(log2(upper_border))
+print(f'Угадай, какое число было загадано? У тебя {attempt} попытки', end='')
+temp = attempt + 1
 
 while attempt > 0:
-    user_number = int(input())
+    user_number = get_number(f'\nПопытка №{temp - attempt}: ')
+
     if goal_number < user_number:
         print('Слишком много! ', end='')
     elif goal_number > user_number:
