7bac91e1d9a38080573a641947aa0232e49d6f19
Kowalski42 <sealenochka@gmail.com>
2022-07-26 15:50:30 +0300

    Some additional corrections

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -2,36 +2,47 @@ from random import *
 from math import *
 
 
-def is_valid_digit(string, border):
+def is_valid(string, border):
     return string.isdigit() and 1 <= int(string) <= border
 
 
 def get_number(text):
     while True:
         string = input(text)
-        if is_valid_digit(string, 100):
+        if is_valid(string, 100):
             return int(string)
         else:
             print('Некорректный ввод!', end='')
 
 
-upper_border = get_number('Введи любое число от 1 до 100, в рамках которого будет выбрано случайное: ')
-goal_number, attempt = randint(0, upper_border), ceil(log2(upper_border))
-print(f'Угадай, какое число было загадано? У тебя {attempt} попытки', end='')
-temp = attempt + 1
+def set_goal_number_and_attempt(b):
+    return randint(0, b), ceil(log2(b))
 
-while attempt > 0:
-    user_number = get_number(f'\nПопытка №{temp - attempt}: ')
 
-    if goal_number < user_number:
-        print('Слишком много! ', end='')
-    elif goal_number > user_number:
-        print('Слишком мало! ', end='')
-    else:
-        print('Ура, угадано! ')
-        break
-    attempt -= 1
-    if attempt > 0:
-        print('Попробуй еще раз: ', end='')
+def is_game_win(a, g):
+    temp = 0
+    while a > 0:
+        a -= 1
+        temp += 1
+        user_number = get_number(f'\nПопытка №{temp}: ')
+        if g < user_number:
+            print('Слишком много! ', end='')
+        elif g > user_number:
+            print('Слишком мало! ', end='')
+        else:
+            print('Ура, угадано! ')
+            return True
+        if a > 0:
+            print('Попробуй еще раз: ', end='')
+    return False
 
+
+flag = True
+print('Добро пожаловать в числовую угадайку! Данная игра существует благодаря курсу "Поколение Python" '
+      'https://stepik.org/course/58852')
+while flag:
+    upper_border = get_number('Введи любое число от 1 до 100, в рамках которого будет выбрано случайное: ')
+    goal_number, attempt = set_goal_number_and_attempt(upper_border)
+    print(f'Угадай, какое число было загадано? У тебя {attempt} попытки', end='')
+    flag = is_game_win(attempt, goal_number)
 print('Игра окончена!')
